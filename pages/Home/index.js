3e9d4692477c28888ede6973951a26964c125982
DharoOficial <erickbandeiradharo@gmail.com>
2021-12-05 16:48:57 -0300

    rota criada

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -9,11 +9,11 @@ $(document).ready(function(){
         darkTheme = !darkTheme
         if(darkTheme){
             $("body").removeClass("lightTheme").addClass("darkTheme")
-            $("button").removeClass("botao_projeto").addClass("botao_projeto_dark")
+            $(".botao_projeto").removeClass("botao_projeto").addClass("botao_projeto_dark")
             $(".cabecalho").css("background","#1a4077");
         } else {
             $("body").removeClass("darkTheme").addClass("lightTheme")
-            $("button").removeClass("botao_projeto_dark").addClass("botao_projeto")
+            $(".botao_projeto").removeClass("botao_projeto_dark").addClass("botao_projeto")
             $(".cabecalho").css("background","#3d6299");
         }
     });
